3c2f00819ef054f79643a31a594e88893ac58ef6
GiancarloRodriguezCS <105930315+GiancarloRodriguezCS@users.noreply.github.com>
2022-05-20 10:08:14 -0400

    Add files via upload

---
+++ Wordsle.java
@@ -0,0 +1,12 @@
+package wordsle;
+
+public class Wordsle {
+
+
+    public static void main(String[] args) {
+        System.out.print(Math.random());
+        mainScreen o = new mainScreen();
+        o.setVisible(true);
+    }
+
+}
